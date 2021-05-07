2135cea9261686237bd7bfb7325a505192a3f026
赵 <eqkiller@163.com>
2021-05-07 10:57:29 +0800

    Add files via upload
    循环读文档通过分号分隔符输入自动调用接口。

---
+++ Shell/register.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ 
+FILENAME=account
+
+function While_read_LINE(){
+#读取行内容
+cat $FILENAME | while read LINE
+do
+echo "$LINE"
+
+##拆分字符串到数组
+str=$LINE
+
+IFS=";"
+arr=($str)
+
+s1=${arr[0]}
+s2=${arr[1]}
+
+curl -G -d "country=78&email=${s1}&verificationcode=123123&password=${s2}"  https://www.moonxbt.com/account/app/user_registered/registered >./123.txt
+done
+}
+While_read_LINE
